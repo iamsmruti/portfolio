3bb5c80d4a56ad49554ff7439ee2ca5eba2c49d8
iamsmruti <smrutiranjanbadatya2@gmail.com>
2023-07-26 00:43:43 +0530

    updated resume

--- src/components/Sections/Home/HeroSection.js
+++ src/components/Sections/Home/HeroSection.js
@@ -13,7 +13,7 @@ const HeroSection = () => {
         <p className='md:text-[32px] text-[24px] uppercase font-thin'>I am <span className='font-normal'>Smruti Ranjan</span></p>
         <Type />
 
-        <Link target='_blank' href={"/Smruti_Ranjan_Badatya_MERN_CV.pdf"}>
+        <Link target='_blank' href={"/Smruti Ranjan Badatya.pdf"}>
           <button className='px-4 py-2 dark:border-slate-800 border-slate-400 border-[1px] mt-5'>My Resume</button>
         </Link>
       </div>
