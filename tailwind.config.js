7e500d494f0fceae5dbf49a8b54c75a548116260
Smruti Ranjan Badatya <smrutiranjanbadatya2@gmail.com>
2024-02-26 20:53:31 +0530

    [UPDATE] Dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        black: '#010400',
-        blackOlive: '#30332E',
-        white: '#FFFBFC',
-        blue: '#62BBC1',
-        pink: '#EC058E'
-      }
+        black: "#010400",
+        blackOlive: "#30332E",
+        white: "#FFFBFC",
+        blue: "#62BBC1",
+        pink: "#EC058E",
+      },
     },
   },
   plugins: [],
-}
-
+};
