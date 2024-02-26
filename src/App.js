5ef697416ca618b4ba92beca32caa9c7f60b09f8
Smruti Ranjan Badatya <smrutiranjanbadatya2@gmail.com>
2024-02-26 20:24:36 +0530

    [UPDATE] Route Setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
+import Home from "./pages/Home"
+import Blog from "./pages/Blog"
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
